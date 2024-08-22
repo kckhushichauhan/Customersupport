dfcd0a7c630287b8a845e1d5c265fd10242d0f14
kckhushichauhan <kc.shreegajanan@gmail.com>
2024-08-23 00:33:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Help from './components/help/component';
+
+class App extends Component {
+  render() {
+    return (
+      <Help></Help>
+    );
+  }
+}
+
+export default App;
